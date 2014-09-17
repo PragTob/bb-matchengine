049dd06bc932a791e46fb56d97096d6e0d03e720
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-09-17 14:10:00 +0200

    Some squad checks

--- lib/bb_matchengine/squad.rb
+++ lib/bb_matchengine/squad.rb
@@ -1,12 +1,29 @@
 module BBMatchengine
   class Squad
 
-    attr_reader :team_name
+    attr_reader :team_name, :active_players, :substitutes
+    ACTIVE_PLAYERS_COUNT = 5
+    MAX_SUBSTITUTES      = 7
+
+    class WrongNumberOfActivePlayers < ArgumentError
+      def message
+        "There have to be #{ACTIVE_PLAYERS_COUNT} ready to play."
+      end
+    end
+    class TooManySubstitutesError < ArgumentError ; end
 
     def initialize(team_name, starters, substitutes)
       @team_name      = team_name
       @active_players = starters
       @substitutes    = substitutes
+
+      check_squad
+    end
+
+    private
+    def check_squad
+      raise WrongNumberOfActivePlayers if active_players.size != ACTIVE_PLAYERS_COUNT
+      raise TooManySubstitutesError if @substitutes.size > MAX_SUBSTITUTES
     end
   end
 end
--- spec/bb_matchengine/squad_spec.rb
+++ spec/bb_matchengine/squad_spec.rb
@@ -3,12 +3,45 @@ require 'spec_helper'
 describe BBMatchengine::Squad do
 
   let(:name) {'Red Dragons'}
-  let(:starters) {[PlayerFactory.create]}
-  let(:substitutes) {[]}
+  let(:starters) {PlayerFactory.mass_create 5}
+  let(:substitutes) {PlayerFactory.mass_create 7}
 
   subject {described_class.new name, starters, substitutes}
 
   it 'has the right name' do
     expect(subject.team_name).to eq (name)
+  end
+
+  it 'has the starters as active players' do
+    expect(subject.active_players).to eq starters
+  end
+
+  it 'has the substitutes' do
+    expect(subject.substitutes).to eq substitutes
+  end
+
+  describe 'one starter too few' do
+    let(:starters) {PlayerFactory.mass_create 4}
+
+    it 'raises an error' do
+      expect {subject}.to raise_error BBMatchengine::Squad::WrongNumberOfActivePlayers
+    end
+  end
+
+  describe 'one starter too much' do
+    let(:starters) {PlayerFactory.mass_create 6}
+
+    it 'raises an error' do
+      expect {subject}.to raise_error BBMatchengine::Squad::WrongNumberOfActivePlayers
+    end
+  end
+
+  describe 'too many substitutes' do
+    let(:substitutes) {PlayerFactory.mass_create 8}
+
+    it 'raises an error' do
+      expect {subject}.to raise_error BBMatchengine::Squad::TooManySubstitutesError
+    end
+
   end
 end
