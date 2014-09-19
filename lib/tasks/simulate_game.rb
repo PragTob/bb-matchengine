3f8acd16d6ff825d1d85a03050a4a321e4bae88e
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-09-19 22:15:32 +0200

    rake task adjusted to take number of simulations to run

--- lib/tasks/simulate_game.rb
+++ lib/tasks/simulate_game.rb
@@ -3,11 +3,14 @@ require_relative '../../spec/bb_matchengine/helpers/player_factory'
 require_relative '../../spec/bb_matchengine/helpers/squad_factory'
 
 desc 'Simulate a dummy game'
-task :simulate_game do
+task :simulate_game, [:number] do |task, args|
+  number = args[:number].to_i || 1
   squad_1 = SquadFactory.create
   squad_2 = SquadFactory.create
-  game = BBMatchengine::Game.new squad_1, squad_2
-  game.play
-  puts game.team_a_score
-  puts game.team_b_score
+
+  number.times do
+    game = BBMatchengine::Game.new squad_1, squad_2
+    game.play
+    puts "#{game.team_a_score}:#{game.team_b_score}"
+  end
 end
