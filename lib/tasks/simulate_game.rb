9e5212eda7d26b4820b7d59a20bad89b1ff6c2a4
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-09-19 22:20:22 +0200

    Show average game score on simulation task

--- lib/tasks/simulate_game.rb
+++ lib/tasks/simulate_game.rb
@@ -8,9 +8,16 @@ task :simulate_game, [:number] do |task, args|
   squad_1 = SquadFactory.create
   squad_2 = SquadFactory.create
 
+  home_total_score = 0
+  away_total_score = 0
+
   number.times do
     game = BBMatchengine::Game.new squad_1, squad_2
     game.play
+    home_total_score += game.team_a_score
+    away_total_score += game.team_b_score
     puts "#{game.team_a_score}:#{game.team_b_score}"
   end
+
+  puts "Average score: #{home_total_score/number}:#{away_total_score/number}"
 end
