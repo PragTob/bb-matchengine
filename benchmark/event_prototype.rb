76c2989cf8552a772ca7bf74f66dfc963c2a3732
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2014-09-30 17:26:51 +0200

    A small benchmark of two event system prototypes

---
+++ benchmark/event_prototype.rb
@@ -0,0 +1,75 @@
+class ObjectEventPublisher
+  def initialize
+    @event_to_subscribers = Hash.new []
+  end
+
+  def subscribe(event, &subscriber_block)
+    @event_to_subscribers[event] << subscriber_block
+  end
+
+  def emit(event)
+    @event_to_subscribers[event.class].each do |block|
+      block.call(event)
+    end
+  end
+end
+
+class SimpleEvent
+  attr_reader :a, :b
+
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end
+end
+
+class ObjectEventSubscriber
+  def initialize(publisher)
+    publisher.subscribe(SimpleEvent) do |event| event.a + event.b end
+  end
+end
+
+object_publisher = ObjectEventPublisher.new
+object_subscriber = ObjectEventSubscriber.new object_publisher
+object_publisher.emit(SimpleEvent.new 1, 2)
+
+
+class SymbolEventPublisher
+  def initialize
+    @event_to_subscribers = Hash.new []
+  end
+
+  def subscribe(event, subscriber)
+    @event_to_subscribers[event] << subscriber
+  end
+
+  def emit(event, *args)
+    @event_to_subscribers[event].each do |subscriber|
+      subscriber.public_send(event, *args)
+    end
+  end
+end
+
+class SymbolEventSubscriber
+  def initialize(publisher)
+    publisher.subscribe :simple_event, self
+  end
+
+  def simple_event(a, b)
+    a + b
+  end
+end
+
+symbol_publisher = SymbolEventPublisher.new
+symbol_subscriber = SymbolEventSubscriber.new symbol_publisher
+symbol_publisher.emit :simple_event, 1, 2
+
+require 'benchmark/ips'
+
+Benchmark.ips do |benchmark|
+  benchmark.config(warmup: 10, time: 20)
+
+  benchmark.report('object') {object_publisher.emit(SimpleEvent.new 1, 2) }
+  benchmark.report('symbol') {symbol_publisher.emit :simple_event, 1, 2 }
+end
+
